d0a30b4672ef18a23b1f7c6e91a1c3d569e24236
wojnat12q <wojteknato@gmail.com>
2025-01-21 14:03:06 +0100

    update

---
+++ src/main/java/guru/springframework/sftgi/SftgiApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sftgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftgiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sftgi/SftgiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sftgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
